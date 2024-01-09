9c6d71798b996bd0f4af251dbb72fa837eca6422
stevending1st <stevending1st@163.com>
2024-01-10 00:10:17 +0800

    feat(Lecture08-demo): 添加实战项目本讲代码（项目初始化）

---
+++ Project/frontEnd/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'MBTI',
+  description: 'A project of a MBTI stored block chain',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
