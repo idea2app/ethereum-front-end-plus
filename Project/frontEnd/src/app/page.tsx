1e93b07973f14e03f154c96000f486c61c59f701
stevending1st <stevending1st@163.com>
2024-01-10 14:28:11 +0800

    fix(Lecture08-demo): delete unless useless file

--- Project/frontEnd/src/app/page.tsx
+++ Project/frontEnd/src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Container } from 'react-bootstrap';
 
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <Container as="main" className={`${styles.main}`}>
+    <Container as="main">
       <h1 className='text-center mt-5 mb-3'>MBTI</h1>
     </Container>
   )
