91297112641cb708ea0a7e9cd876af0ca1c608e1
stevending1st <stevending1st@163.com>
2024-01-12 21:36:34 +0800

    feat(Lecture09-demo): 更新项目登录相关业务

---
+++ Project/frontEnd/src/models/MetaMask.ts
@@ -0,0 +1,16 @@
+class MetaMask {
+  constructor() {
+    window?.ethereum?.on('accountsChanged', () => location.reload());
+  }
+
+  async connectWallet() {
+    if (!window?.ethereum) throw new Error('MetaMask is not installed!');
+
+    const [account] = (await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })) as string[];
+    return account;
+  }
+}
+
+export default new MetaMask();
